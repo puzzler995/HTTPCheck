4b76f501389e9ec1aff9bd38a00bae6de4959cca
puzzler995 <ksmarsee@gmail.com>
2023-01-06 23:48:13 -0500

    Basic Sidebar Update

--- HTTPCheck.py
+++ HTTPCheck.py
@@ -6,8 +6,10 @@ class HttpCheckApp:
     self.mainLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=2)
     self.master.setLayout(self.mainLayout)
     self.sideBarLayout = ttk.TTkVBoxLayout()
-    self.sideBarLayout.addWidget(ttk.TTkButton(border=True, text="Sidebar1"))
-    self.sideBarLayout.addWidget(ttk.TTkButton(border=True, text="Sidebar2"))
+    header = ttk.TTkLabel(text="HTTP Status Checker", color=ttk.TTkColor.BOLD+ttk.TTkColor.UNDERLINE)
+    self.sideBarLayout.addWidget(header)
+    self.sideBarLayout.addWidget(ttk.TTkButton(border=False, text="Sidebar1"))
+    self.sideBarLayout.addWidget(ttk.TTkButton(border=False, text="Sidebar2"))
     self.mainLayout.addItem(self.sideBarLayout,0,0,4,1)
     for row in range(0,4):
       for col in range(2,6):
