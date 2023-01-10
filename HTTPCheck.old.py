a4622223bcf5897a49c9e8b4c6f87d3d27e58e0c
puzzler995 <ksmarsee@gmail.com>
2023-01-09 22:10:06 -0500

    Moving to tkinter because I need documentation

---
+++ HTTPCheck.old.py
@@ -0,0 +1,96 @@
+import datetime, yaml
+from pathlib import Path
+import TermTk as ttk
+
+class Site:
+  def __init__(self, name: str, url: str, lastupdate, laststatus) -> None:
+    self.name = name
+    self.url = url
+    self.lastupdate = lastupdate
+    self.laststatus = laststatus
+    self.frame = None
+
+class SiteFrame(ttk.TTkFrame):
+  def __init__(self,parent, site:Site, *args, **kwargs):
+    ttk.TTkFrame.__init__(self,title=site.name, *args, **kwargs)
+    self.parent:HttpCheckApp = parent
+    self.site = site
+    self.urllabel = ttk.TTkLabel(parent=self, pos=(0,0), text=site.url)
+    self.statuslabel = ttk.TTkLabel(parent=self, pos=(0,1), text=site.laststatus)
+    self.lastupdatelabel = ttk.TTkLabel(parent=self, pos=(0,2), text=site.lastupdate)
+    self.editbutton = ttk.TTkButton(parent=self, pos=(0,3), border=False, text="Edit")
+    self.editbutton.clicked.connect(lambda: self.editSite())
+    self.deletebutton = ttk.TTkButton(parent=self, pos=(0,4), border=False, text="Delete")
+    self.deletebutton.clicked.connect(self.deleteSite)
+    self.setMinimumHeight(7)
+    self.setMinimumWidth(15)
+    self.site.frame = self
+  def deleteSite(self):
+    self.parent.deleteSite(self.site)
+  def editSite(self):
+    editWindow = ttk.TTkWindow(pos = (3,3), size=(30,10), title="Edit")
+    ttk.TTkLabel(parent=editWindow, pos=(5,5), text="Hello World")
+
+
+class HttpCheckApp:
+  def __init__(self, master: ttk.TTk) -> None:
+    self.master=master
+    self.dev = False
+    self.refreshTime = 0
+    self.siteList = []
+    self.mainLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=2)
+    self.master.setLayout(self.mainLayout)
+    self.sideBarLayout = ttk.TTkVBoxLayout()
+    header = ttk.TTkLabel(text="HTTP Status Checker", color=ttk.TTkColor.BOLD+ttk.TTkColor.UNDERLINE)
+    self.sideBarLayout.addWidget(header)
+    self.sideBarLayout.addWidget(ttk.TTkButton(border=False, text="Sidebar1"))
+    quitBtn = ttk.TTkButton(border=False, text="Exit")
+    quitBtn.clicked.connect(self.quit)
+    self.sideBarLayout.addWidget(quitBtn)
+    self.mainLayout.addItem(self.sideBarLayout,0,0,4,1)
+    self.loadConfig()
+    currow = 0
+    curcol = 2
+    for site in self.siteList:
+      self.mainLayout.addWidget(SiteFrame(self, site), currow, curcol)
+      curcol += 1
+      if curcol == 6:
+        currow += 1
+        curcol = 2
+    print("Done!")
+  def loadConfig(self):
+    #TODO: Actually Change this to config file location
+    configfile = Path.cwd() / "test" / "config.yaml"
+    with configfile.open() as file:
+      config = yaml.safe_load(file)["config"]
+    try: #This is a try block because I dont intend to leave the dev variable in prod versions of the config file
+      self.dev = config["dev"]
+    except:
+      pass
+    self.refreshTime = config["refreshtime"]
+    if config["sites"] is not None:
+      for site in config["sites"]:
+        self.siteList.append(Site(site["name"], site["url"], site["lastupdate"], site["laststatus"]))
+  def saveConfig(self):
+    #TODO: Actually Change this to config file location
+    configfile = Path.cwd() / "test" / "config.yaml"
+    sites = []
+    site: Site
+    for site in self.siteList:
+      sites.append({"name": site.name, "url": site.url, "lastupdate": site.lastupdate, "laststatus": site.laststatus})
+    config = {"config": {"dev": self.dev, "refreshtime": self.refreshTime, "sites": sites}}
+    
+    with configfile.open("w") as file:
+      yaml.dump(config, file)
+  def quit(self):
+    self.saveConfig()
+    ttk.TTkHelper.quit()
+  def deleteSite(self, site: Site):
+    self.siteList.remove(site)
+    self.mainLayout.removeWidget(site.frame)
+    self.saveConfig()
+
+# if __name__=="main":
+root = ttk.TTk()
+App = HttpCheckApp(root)
+App.master.mainloop() 
--- HTTPCheck.py
+++ HTTPCheck.py
@@ -1,6 +1,7 @@
-import datetime, yaml
+import tkinter as tk
+import yaml, datetime
+from tkinter import ttk
 from pathlib import Path
-import TermTk as ttk
 
 class Site:
   def __init__(self, name: str, url: str, lastupdate, laststatus) -> None:
@@ -8,53 +9,84 @@ class Site:
     self.url = url
     self.lastupdate = lastupdate
     self.laststatus = laststatus
-    self.frame = None
+    self.frame:SiteFrame = None
 
-class SiteFrame(ttk.TTkFrame):
-  def __init__(self, site:Site, *args, **kwargs):
-    ttk.TTkFrame.__init__(self,title=site.name, *args, **kwargs)
+class SiteFrame(ttk.LabelFrame):
+  def __init__(self, container, site:Site, *args, **kwargs):
+    super().__init__(container, text=site.name, *args, **kwargs)
     self.site = site
-    self.urllabel = ttk.TTkLabel(parent=self, pos=(0,0), text=site.url)
-    self.statuslabel = ttk.TTkLabel(parent=self, pos=(0,1), text=site.laststatus)
-    self.lastupdatelabel = ttk.TTkLabel(parent=self, pos=(0,2), text=site.lastupdate)
-    self.editbutton = ttk.TTkButton(parent=self, pos=(0,3), border=False, text="Edit")
-    self.deletebutton = ttk.TTkButton(parent=self, pos=(0,4), border=False, text="Delete")
-    self.setMinimumHeight(7)
-    self.setMinimumWidth(15)
+    self.urllabel = ttk.Label(self, text=self.site.url)
+    self.urllabel.pack()
+    self.statuslabel = ttk.Label(self, text=self.site.laststatus)
+    self.statuslabel.pack()
+    self.lastupdatelabel = ttk.Label(self, text=self.site.lastupdate)
+    self.lastupdatelabel.pack()
+    self.editbutton = ttk.Button(
+      self,
+      text="Edit"
+      #TODO: Edit Command
+    )
+    self.editbutton.pack()
+    self.deletebutton = ttk.Button(
+      self,
+      text="Delete",
+      command = lambda: self.deleteFrame()
+    )
+    self.deletebutton.pack()
     self.site.frame = self
+  def deleteFrame(self):
+    self.master.master.deleteSite(self.site)
 
-class HttpCheckApp:
-  def __init__(self, master: ttk.TTk) -> None:
-    self.master=master
+
+
+
+class HttpCheckApp(tk.Tk):
+  def __init__(self) -> None:
+    super().__init__()
+    self.title("HTTP Status Checker")
     self.dev = False
     self.refreshTime = 0
     self.siteList = []
-    self.mainLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=2)
-    self.master.setLayout(self.mainLayout)
-    self.sideBarLayout = ttk.TTkVBoxLayout()
-    header = ttk.TTkLabel(text="HTTP Status Checker", color=ttk.TTkColor.BOLD+ttk.TTkColor.UNDERLINE)
-    self.sideBarLayout.addWidget(header)
-    self.sideBarLayout.addWidget(ttk.TTkButton(border=False, text="Sidebar1"))
-    quitBtn = ttk.TTkButton(border=False, text="Exit")
-    quitBtn.clicked.connect(self.quit)
-    self.sideBarLayout.addWidget(quitBtn)
-    self.mainLayout.addItem(self.sideBarLayout,0,0,4,1)
+    self.sidebar = ttk.Frame(self)
+    self.sidebar.pack(side=tk.LEFT, fill=tk.Y)
+    addBtn = ttk.Button(
+      self.sidebar,
+      text = "Add Site"
+    )
+    configBtn = ttk.Button(
+      self.sidebar,
+      text = "Config"
+    )
+    quitBtn = ttk.Button(
+      self.sidebar,
+      text = "Quit",
+      command=lambda: self.quitapp()
+    )
+    quitBtn.pack(side=tk.BOTTOM)
+    configBtn.pack(side=tk.BOTTOM)
+    addBtn.pack(side=tk.BOTTOM)
     self.loadConfig()
-    currow = 0
-    curcol = 2
+    self.siteframe = ttk.Frame(self)
+    self.curx = 0
+    self.cury = 0
     for site in self.siteList:
-      self.mainLayout.addWidget(SiteFrame(site), currow, curcol)
-      curcol += 1
-      if curcol == 6:
-        currow += 1
-        curcol = 2
+      sframe = SiteFrame(self.siteframe, site)
+      sframe.grid(column=self.curx, row=self.cury, padx=5, pady=5)
+      self.curx += 1
+      if self.curx == 4:
+        self.cury += 1
+        self.curx = 0
+    self.siteframe.pack()
     print("Done!")
+
   def loadConfig(self):
-    #TODO: Actually Change this to config file location
+    #TODO: Actually change this to config file location
     configfile = Path.cwd() / "test" / "config.yaml"
+    sites = []
+    site: Site
     with configfile.open() as file:
       config = yaml.safe_load(file)["config"]
-    try: #This is a try block because I dont intend to leave the dev variable in prod versions of the config file
+    try: 
       self.dev = config["dev"]
     except:
       pass
@@ -63,21 +95,28 @@ class HttpCheckApp:
       for site in config["sites"]:
         self.siteList.append(Site(site["name"], site["url"], site["lastupdate"], site["laststatus"]))
   def saveConfig(self):
-    #TODO: Actually Change this to config file location
+    #TODO: Actually change this to config file location
     configfile = Path.cwd() / "test" / "config.yaml"
     sites = []
     site: Site
     for site in self.siteList:
       sites.append({"name": site.name, "url": site.url, "lastupdate": site.lastupdate, "laststatus": site.laststatus})
     config = {"config": {"dev": self.dev, "refreshtime": self.refreshTime, "sites": sites}}
-    
     with configfile.open("w") as file:
       yaml.dump(config, file)
-  def quit(self):
+  def quitapp(self):
     self.saveConfig()
-    ttk.TTkHelper.quit()
+    self.quit()
+  def deleteSite(self, site: Site):
+    self.siteList.remove(site)
+    site.frame.destroy()
+    if self.curx ==0:
+      if self.cury != 0:
+        self.cury -= 1
+        self.curx = 3
+    else:
+      self.curx -= 1
+    self.saveConfig()
 
-# if __name__=="main":
-root = ttk.TTk()
-App = HttpCheckApp(root)
-App.master.mainloop() 
+app = HttpCheckApp()
+app.mainloop()
